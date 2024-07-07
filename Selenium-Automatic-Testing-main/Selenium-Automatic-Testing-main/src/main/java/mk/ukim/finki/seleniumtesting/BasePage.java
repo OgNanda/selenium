f41f30ceae3c62b40b5f1941fb05bfe6dc89226f
OgNanda <123239411+OgNanda@users.noreply.github.com>
2024-07-07 12:23:51 +0530

    FB login test with selenium
    Facebook Login Page Automation with Selenium in Java
    
    This project involves automating the login process for Facebook using Selenium WebDriver. The script tests various login scenarios:
    
    Correct Username and Correct Password:
    
    Navigate to Facebook's login page.
    Enter a valid username and password.
    Verify successful login.
    Incorrect Username and Correct Password:
    
    Enter an invalid username with a valid password.
    Verify the error message for incorrect username.
    Incorrect Username and Incorrect Password:
    
    Enter an invalid username and password.
    Verify the error message for incorrect credentials.
    Correct Username and Incorrect Password:
    
    Enter a valid username with an invalid password.
    Verify the error message for incorrect password.
    The script includes implicit waits to handle page load times and ensure elements are interactable before performing actions.

---
+++ Selenium-Automatic-Testing-main/Selenium-Automatic-Testing-main/src/main/java/mk/ukim/finki/seleniumtesting/BasePage.java
@@ -0,0 +1,15 @@
+package mk.ukim.finki.seleniumtesting;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+    public WebDriverWait wait;
+    protected WebDriver driver;
+
+    @SuppressWarnings("deprecation")
+	public BasePage(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, 10);
+    }
+}
